24d9a03a398b340dd41ba863166f5a8bf0470944
Ryan Bollettino <ryan.bollettino@gmail.com>
2022-07-15 22:56:45 -0500

    init App file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import './App.css';
+import {React, useState, useEffect} from 'react'
+import ContextProvider from './ContextProvider';
+import {Routes, Route} from 'react-router'
+import Navbar from './components/Navbar';
+import GamesContainer from './components/GamesContainer'
+// import MyCollection from './components/MyCollection'
+// import AddGame from './components/AddGame'
+import ShowGame from './components/ShowGame'
+// import SearchContainer from './components/SearchContainer'
 
 function App() {
+   
+
   return (
     <div className="App">
-      HELLO
+    <ContextProvider>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<GamesContainer />} />
+        {/* <Route exact path="/myCollection" element={ <MyCollection  />}  /> */}
+        {/* <Route exact path="/add-game" element={ <AddGame />}  /> */}
+        <Route exact path="/games/:id" element={ <ShowGame />}  />
+        {/* <Route exact path="/search" element={ <SearchContainer />}  /> */}
+      </Routes>
+    </ContextProvider>
     </div>
-  );
+  )
 }
 
 export default App;
