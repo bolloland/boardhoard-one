1ac91be41f90fe24d5d268a86f4b133a6e6d3a4f
Ryan Bollettino <ryan.bollettino@gmail.com>
2022-07-15 22:59:20 -0500

    init GameTitle comp

---
+++ src/components/GameTitle.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const GameTitle = (props) => {
+    return (
+        <div>
+            <b>{props.gamedata.name}</b>
+        </div>
+    )
+}
+export default GameTitle
